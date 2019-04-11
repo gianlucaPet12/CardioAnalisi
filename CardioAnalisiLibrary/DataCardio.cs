d5e493e4c034ec2d71db8fb9b9b3ab201e4d7009
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-11 09:57:37 +0200

    Creati i metodi per i punti 2 e 3 ed i relativi test

--- CardioAnalisiLibrary/DataCardio.cs
+++ CardioAnalisiLibrary/DataCardio.cs
@@ -8,5 +8,40 @@ namespace CardioAnalisiLibrary
 {
     public class DataCardio
     {
+        public static string TipoBattio(int battiti, string tipo)
+        {
+            string rispo = "";
+            if (battiti < 60)
+            {
+                rispo = "Bradicardia";
+            }
+
+            if (battiti >= 60 && battiti <= 100)
+            {
+                rispo = "Normale";
+            }
+
+            if (battiti > 100)
+            {
+                rispo = "Tachicardia";
+            }
+
+            return rispo;
+        }
+        public static double CalcoloCalorie(double anni, double peso, double frequenza, double tempo, string sesso)
+        {
+            double calorie = 0;
+            if (sesso == "uomo")
+            {
+                calorie= ((anni * 0.2017) + (peso * 0.199) +(frequenza * 0.6309) - 55.0969) * tempo / 4.184;
+            }
+
+            if (sesso == "donna")
+            {
+                calorie = ((anni * 0.074) + (peso * 0.126) + (frequenza * 0.4472) - 20.4022) * tempo / 4.184;
+            }
+
+            return calorie;
+        }
     }
 }
--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -6,9 +6,28 @@ namespace DataCardio.Test
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow (55, "Bradicardia")] 
+        [DataRow (60, "Normale")]
+        [DataRow (101, "Tachicardia")]
+        public void TestMethod2(int battiti, string tipo)
         {
+         string var;
+
+         var = CardioAnalisiLibrary.DataCardio.TipoBattio(battiti, tipo);
+            Assert.AreEqual(var, tipo);
+        }
+
+        [DataTestMethod]
+        [DataRow(20, 70, 80, 60, "uomo", 191)]
+        [DataRow(16, 50, 70, 40, "donna", 176)]    
+        public void TestMethod3(double anni, double peso, double frequenza, double tempo, string sesso, int ris_atteso)
+        {
+            int var;
+
+            var =Convert.ToInt32( CardioAnalisiLibrary.DataCardio.CalcoloCalorie(anni, peso, frequenza, tempo, sesso));
+
+            Assert.AreEqual(ris_atteso, var);
         }
     }
 }
